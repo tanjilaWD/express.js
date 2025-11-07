07da3b423f31fc2517d33593bc38cadee513b176
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-11-07 21:23:03 +0600

    http response

--- Lesson1/app.js
+++ Lesson1/app.js
@@ -4,6 +4,34 @@ const userRouter = require('./routes/users.route');
 
 app.use("/api/user", userRouter);
 
+app.use('/register', (req, res)=>{
+   // res.status(200).json({
+   //    'name': 'Tanjila khatun',
+   //    'message': 'I am register page',
+   //    statusCode: 200
+   // });
+
+
+   // res.redirect('/login');
+
+
+   res.statusCode = 200;
+   res.sendFile(__dirname+ "/views/register.html");
+});
+
+app.use("/", (req,res) =>{
+   res.statusCode = 200 ;
+   res.sendFile(__dirname+ "/views/index.html");
+});
+
+app.get("/login",(req,res) =>{
+   // res.cookie("name", "Tanjila");
+   // res.cookie("age", "20");
+   res.clearCookie("name");
+   res.append("id", "130000");
+   res.end();
+});
+
 app.use('/',(req, res)=>{
    res.send('I am a get request at home route'); 
    res.end();
