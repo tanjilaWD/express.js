b5864672c15ecd34caee02447c8143544864686b
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-11-07 19:37:31 +0600

    route

--- Lesson1/app.js
+++ Lesson1/app.js
@@ -1,29 +1,32 @@
 const express = require('express');
 const app = express();
+const userRouter = require('./routes/users.route');
 
-app.get('/',(req, res)=>{
+app.use("/api/user", userRouter);
+
+app.use('/',(req, res)=>{
    res.send('I am a get request at home route'); 
    res.end();
 } );
 
-// app.get('/about',(req, res)=>{
-//    res.send('I am a get request at about route'); 
+app.use((req, res) =>{
+   res.send('<h2>404 ! Not a valid url</h2>')
+})
+ 
+// app.post('/',(req, res)=>{
+//    res.send('I am a post request at home route'); 
+//    res.end();
 // } );
 
-app.post('/',(req, res)=>{
-   res.send('I am a post request at home route'); 
-   res.end();
-} );
+// app.put('/',(req, res)=>{
+//    res.send('I am a put request at home route'); 
+//    res.end();
+// } );
 
-app.put('/',(req, res)=>{
-   res.send('I am a put request at home route'); 
-   res.end();
-} );
-
-app.delete('/',(req, res)=>{
-   res.send('I am a delete request at home route'); 
-   res.end();
-} );
+// app.delete('/',(req, res)=>{
+//    res.send('I am a delete request at home route'); 
+//    res.end();
+// } );
 
 
 
---
+++ Lesson1/routes/users.route.js
@@ -0,0 +1,15 @@
+
+const express = require('express');
+const router = express.Router();
+
+
+
+router.get('/register',(req, res)=>{
+   res.send('I am a get request at register route'); 
+} );
+router.get('/login',(req, res)=>{
+   res.send('<h1>I am a get request at login route</h1>'); 
+} );
+
+
+module.exports = router;
