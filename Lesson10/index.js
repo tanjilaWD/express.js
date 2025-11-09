43add127a3fabacf2aac973d71ceefa227f60378
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-11-09 23:53:17 +0600

    static middleware

---
+++ Lesson10/index.js
@@ -0,0 +1,15 @@
+//express static middleware
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req,res)=>{
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(PORT, (req,res)=>{
+    console.log(`Server is running at http://localhost:${PORT}`);
+})
